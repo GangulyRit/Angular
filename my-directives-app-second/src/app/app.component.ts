f719b4b7cd3fe719c59b589b76bf16f6fd48dcc6
GangulyRit <44023607+GangulyRit@users.noreply.github.com>
2018-11-20 21:44:42 +0530

    Add files via upload
    Initial commit

--- my-directives-app-second/src/app/app.component.ts
+++ my-directives-app-second/src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-directives-app-second';
+
+    coursesArr = [];   //For Hidden property
+
+    viewMode = 'map';   //For [ngSwitch]
+
+    courses =[
+        { id: 1, name: 'course1'},
+        { id: 2, name: 'course2'},
+        { id: 3, name: 'course3'}
+    ];
+
+    coursesN = [];
+
+    onAdd(){
+        this.courses.push({id: 4, name: 'course4'});
+    }
+
+    onRemove(course){
+        let index = this.courses.indexOf(course);
+        this.courses.splice(index, 1);
+    }
+
+    loadCourses(){
+        this.coursesN = [
+            { id: 1, name: 'course1'},
+            { id: 2, name: 'course2'},     //For trackBy
+            { id: 3, name: 'course3'}
+        ]
+    }
+
+    trackCourse(course) {
+        return course ? course.id : undefined;  //For trackBy
+    }
+
+    isSelected = false; //For [ngClass]   TODo
+
+    onClick() {
+        this.isSelected = !this.isSelected;   //For [ngClass]     TODo
+    }
+
+    canSave = true;   //For [ngStyle]
+
+    task = {
+        title: 'Reiew Applications',    //For Safe Traversal Operator
+        assignee: null
+    }
+
+
 }
