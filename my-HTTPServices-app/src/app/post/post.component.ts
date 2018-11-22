418bf40c39d985d7067deb3ede4d87c156abf319
GangulyRit <44023607+GangulyRit@users.noreply.github.com>
2018-11-22 17:19:24 +0530

    Initial commit

--- my-HTTPServices-app/src/app/post/post.component.ts
+++ my-HTTPServices-app/src/app/post/post.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+//import { Http } from '@angular/http';
 import { Http } from '@angular/http';
-import {subscribeTo} from "rxjs/internal-compatibility";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
+
 export class PostComponent implements OnInit {
 
-  constructor(http: Http) {
+    constructor(private http: Http) {
+        http.get('http://jsonplaceholder.typicode.com/posts')
+            .subscribe(response => {
+                console.log(response.json());
+            });
+    }
 
-    http.get('http://jsonplaceholder.typicode.com/posts')
-      .subscribe(response => {
-        console.log(response);
-      });
-  }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
 
 }
