607b50d6936db7461c39522af8607688c6626f43
GangulyRit <44023607+GangulyRit@users.noreply.github.com>
2019-04-12 17:40:12 +0530

    Initial commit

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { environment } from "./../environments/environment";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- oshop/src/environments/environment.ts
+++ oshop/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBbUsyhkYiYVxgTpiNgFwmPVYYbVWl0Rwo",
+    authDomain: "oshop-9d342.firebaseapp.com",
+    databaseURL: "https://oshop-9d342.firebaseio.com",
+    projectId: "oshop-9d342",
+    storageBucket: "oshop-9d342.appspot.com",
+    messagingSenderId: "562777372767"
+  }
 };
