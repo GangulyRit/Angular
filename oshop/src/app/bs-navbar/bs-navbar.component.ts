2c516781bd899e02f1cfb722561bd43fbd33220d
GangulyRit <44023607+GangulyRit@users.noreply.github.com>
2019-04-24 12:34:05 +0530

    Initial commit

--- oshop/src/app/bs-navbar/bs-navbar.component.ts
+++ oshop/src/app/bs-navbar/bs-navbar.component.ts
@@ -3,10 +3,10 @@ import { OnInit, Component } from '@angular/core';
 Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
-  styleUrls: './bs-navabar.component.css'
+  styleUrls: ['./bs-navabar.component.css']
 })
 
-export class BsnavbarComponent implements OnInit {
+export class BsNavbarComponent implements OnInit {
 
   constructor() {}
 
